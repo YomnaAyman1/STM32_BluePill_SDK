1a4c802f980377fbc4b025552d84a2e5bea363a1
YomnaAyman1 <68161203+YomnaAyman1@users.noreply.github.com>
2023-11-02 05:09:10 +0200

    Update BUTTON_interface.h

--- 03 - HAL/03 - BUTTON/BUTTON_interface.h
+++ 03 - HAL/03 - BUTTON/BUTTON_interface.h
@@ -1,7 +1,7 @@
 /*****************************************************/
- /* Author      : mosad                              */
- /* Version     : v01                                */
- /* date        : 15/8/2020                          */
+/* Author      : yomna                              */
+/* Version     : v01                                */
+/* date        : 4/6/2022                           */
 /*****************************************************/
 #ifndef BUTTON_INTERFACE_H
 #define BUTTON_INTERFACE_H
