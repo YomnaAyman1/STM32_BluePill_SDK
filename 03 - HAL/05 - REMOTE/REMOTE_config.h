0480d9a9680f5a0221a0e3e0f8344290010635c3
YomnaAyman1 <68161203+YomnaAyman1@users.noreply.github.com>
2023-11-02 04:59:30 +0200

    Update REMOTE_config.h

--- 03 - HAL/05 - REMOTE/REMOTE_config.h
+++ 03 - HAL/05 - REMOTE/REMOTE_config.h
@@ -1,7 +1,7 @@
 /*****************************************************/
- /* Author      : mosad                              */
- /* Version     : v01                                */
- /* date        : 4/9/2020                          */
+/* Author      : yomna                              */
+/* Version     : v01                                */
+/* date        : 4/6/2022                           */
 /*****************************************************/
 #ifndef REMOTE_CONFIG_H
 #define REMOTE_CONFIG_H
