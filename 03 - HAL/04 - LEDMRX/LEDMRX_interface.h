5b7b474bc48c727bd985266f1b5f3ef98be0eb0d
YomnaAyman1 <68161203+YomnaAyman1@users.noreply.github.com>
2023-11-02 05:07:12 +0200

    Update LEDMRX_interface.h

--- 03 - HAL/04 - LEDMRX/LEDMRX_interface.h
+++ 03 - HAL/04 - LEDMRX/LEDMRX_interface.h
@@ -1,7 +1,7 @@
 /*****************************************************/
- /* Author      : mosad                              */
- /* Version     : v01                                */
- /* date        : 31/8/2020                          */
+/* Author      : yomna                              */
+/* Version     : v01                                */
+/* date        : 4/6/2022                           */
 /*****************************************************/
 #ifndef LEDMRX_INTERFACE_H
 #define LEDMRX_INTERFACE_H
