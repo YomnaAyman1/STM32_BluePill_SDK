365044384092a4aaf17fe5d4db84eecb4c372e2b
YomnaAyman1 <68161203+YomnaAyman1@users.noreply.github.com>
2023-11-02 05:10:50 +0200

    Update SSD_private.h

--- 03 - HAL/02 - SSD/SSD_private.h
+++ 03 - HAL/02 - SSD/SSD_private.h
@@ -1,7 +1,7 @@
 /*****************************************************/
- /* Author      : mosad                              */
- /* Version     : v01                                */
- /* date        : 14/8/2020                          */
+/* Author      : yomna                              */
+/* Version     : v01                                */
+/* date        : 4/6/2022                           */
 /*****************************************************/
 #ifndef SSD_PRIVATE_H
 #define SSD_PRIVATE_H 
