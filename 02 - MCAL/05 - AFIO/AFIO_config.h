56b83addcb2cb8dde56d182e0f259199abc33220
YomnaAyman1 <68161203+YomnaAyman1@users.noreply.github.com>
2023-11-26 00:00:48 +0200

    Update AFIO_config.h

--- 02 - MCAL/05 - AFIO/AFIO_config.h
+++ 02 - MCAL/05 - AFIO/AFIO_config.h
@@ -1,7 +1,7 @@
 /*****************************************************/
- /* Author      : mosad                              */
- /* Version     : v01                                */
- /* date        : 24/8/2020                          */
+/* Author      : yomna                              */
+/* Version     : v01                                */
+/* date        : 1/6/2022                           */
 /*****************************************************/
 #ifndef AFIO_CONFIG_H
 #define AFIO_CONFIG_H
